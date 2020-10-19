426ec1a19b1ef27d45c7a4f3b5bee4233e50d33d
Bruno <brunosantiagodesouza@outlook.com>
2020-10-18 22:28:07 -0300

    first commit

---
+++ main/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "driver/gpio.h"
+#include "soc/gpio_reg.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+  (*(volatile uint32_t *)GPIO_ENABLE_REG) = 0x02000000;
+ while(1){
+  	(*(volatile uint32_t *)GPIO_OUT_W1TS_REG) = 0x02000000;
+  	printf("led on \n");
+  	vTaskDelay(2000/portTICK_PERIOD_MS);
+  	(*(volatile uint32_t *)GPIO_OUT_W1TC_REG) = 0x02000000;
+  	printf("led off \n");
+	vTaskDelay(2000/portTICK_PERIOD_MS);
+ }
+
+}
