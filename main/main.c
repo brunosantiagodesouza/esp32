1da4875c2272fcc201420af4dc48419140928802
Bruno <brunosantiagodesouza@outlook.com>
2020-10-18 22:56:48 -0300

    kk

--- main/main.c
+++ main/main.c
@@ -4,11 +4,12 @@
 #include "soc/gpio_reg.h"
 #include "freertos/task.h"
 #include "esp_log.h"
+
 void app_main(void)
 {
   (*(volatile uint32_t *)GPIO_ENABLE_REG) = 0x02000000;
  while(1){
-  	(*(volatile uint32_t *)GPIO_OUT_W1TS_REG) = 0x02000000;
+  	(*(volatile uint32_t *)GPIO_OUT_W1TS_REG) = 0x02000000; //access register 
   	printf("led on \n");
   	vTaskDelay(2000/portTICK_PERIOD_MS);
   	(*(volatile uint32_t *)GPIO_OUT_W1TC_REG) = 0x02000000;
