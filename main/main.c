ffeaeecb7add63335a94fb202a08ad4c23a34c9f
Bruno <brunosantiagodesouza@outlook.com>
2020-10-19 22:23:45 -0300

    led pisca apenas quando botao acionado

--- main/main.c
+++ main/main.c
@@ -1,20 +1,27 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "driver/gpio.h"
-#include "soc/gpio_reg.h"
-#include "freertos/task.h"
-#include "esp_log.h"
+#include <stdio.h>                //inclui biblioteca stdio que comporta printf 
+#include <freertos/FreeRTOS.h>	  //inclui FreeRTOS
+#include "soc/gpio_reg.h"		  // inclui biblioteca soc que habilita o trabalho com registradores	
+#include "freertos/task.h"		  // inclui biblioteca task que permite usar delay 
+
 
 void app_main(void)
 {
-  (*(volatile uint32_t *)GPIO_ENABLE_REG) = 0x02000000;
- while(1){
-  	(*(volatile uint32_t *)GPIO_OUT_W1TS_REG) = 0x02000000; //access register 
-  	printf("led on \n");
-  	vTaskDelay(2000/portTICK_PERIOD_MS);
-  	(*(volatile uint32_t *)GPIO_OUT_W1TC_REG) = 0x02000000;
-  	printf("led off \n");
-	vTaskDelay(2000/portTICK_PERIOD_MS);
- }
+	uint32_t result;
+ 
+  	(*(volatile uint32_t *)GPIO_ENABLE_REG) = 0x02000000;  // habilita a gpio 25 como output pelo registrador  
+  	while(1){
+		result =REG_READ(GPIO_IN_REG) & BIT10; // ler todos bits do registrador GPIO_IN_REG
+		if(result == BIT10){	  
+		
+			(*(volatile uint32_t *)GPIO_OUT_W1TS_REG) = 0x02000000; // set gpio 25 
+			printf("led on \n");									
+			vTaskDelay(2000/portTICK_PERIOD_MS);					// Delay de 2000 ms		
+			(*(volatile uint32_t *)GPIO_OUT_W1TC_REG) = 0x02000000; // reset  gpio 25
+			printf("led off \n");
+			vTaskDelay(2000/portTICK_PERIOD_MS);
+		}
+	
+	printf("LED OFF \n");
+ 	}
 
 }
